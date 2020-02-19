2fa74e9930fb796fac01e51cbb34edf609b292c6
mac <machard.1984@gmail.com>
2020-02-19 19:24:37 +0000

    Adding the convert_to_template script

---
+++ pyvmomi/convert_to_template.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+import atexit
+from pyVim.connect import SmartConnectNoSSL, SmartConnect, Disconnect
+from tools import cli
+from pyVmomi import vim
+
+def get_args():
+    parser = cli.build_arg_parser()
+    parser.add_argument('-j', '--uuid',
+                        help='UUID of the VirtualMachine to turn into template')
+    parser.add_argument('-n', '--name',
+                        help='DNS Name of the VirtualMachine to turn into template')
+    parser.add_argument('-i', '--ip',
+                        help='IP Addres sof the Virtual machine to turn into template')
+    my_args = parser.parse_args()
+    return cli.prompt_for_password(my_args)
+
+
+def main():
+    args = get_args()
+    vm = None
+    try:
+        if args.disable_ssl_verification:
+            si = SmartConnectNoSSL(
+                host=args.host,
+                user=args.user,
+                pwd=args.password,
+                port=int(args.port))
+            atexit.register(Disconnect, si)
+        else:
+            si = SmartConnect(
+                host=args.host,
+                user=args.user,
+                pwd=args.password,
+                port=int(args.port))
+            atexit.register(Disconnect, si)
+    except vim.fault.InvalidLogin:
+        raise SystemExit("Unable to connect to host"
+                         "with supplied credentials")
+
+    if args.uuid:
+        vm = si.content.searchIndex.FindByUuid(None, args.uuid, True, True)
+    elif args.name:
+        vm = si.content.searchIndex.FindByDnsName(None, args.name, True)
+    elif args.ip:
+        vm = si.content.searchIndex.FindByIp(None, args.ip, True)
+
+    if vm:
+        vm.MarkAsTemplate()
+    if vm is None:
+        raise SystemExit("Unable to locate VM")
+
+
+if __name__ == "__main__":
+   main()
