ca71818ae9eb03171bd1f8af7c499fbae2e8cd32
root <root@predator.chardma.org.uk>
2020-02-21 22:45:44 +0000

    Reorder the arg sequence

--- api/args.py
+++ api/args.py
@@ -5,14 +5,12 @@ import getpass
 
 
 def build_arg_parser():
-
     parser = argparse.ArgumentParser(
                         description='Standard Arguments for talking to vCenter')
 
-    parser.add_argument('-s', '--host',
-                        required=True,
-                        action='store',
-                        help='vSphere service to connect to')
+    parser.add_argument('-D', '--debug',
+                        action='store_true',
+                        help='enable debugging output')
 
     parser.add_argument('-o', '--port',
                         type=int,
@@ -20,27 +18,28 @@ def build_arg_parser():
                         action='store',
                         help='Port to connect on')
 
-    parser.add_argument('-u', '--user',
-                        required=True,
-                        action='store',
-                        help='User name to use when connecting to host')
+    parser.add_argument('-n', '--name',
+                        help='DNS Name of the VirtualMachine to power on')
 
     parser.add_argument('-p', '--password',
                         required=False,
                         action='store',
                         help='Password to use when connecting to host')
 
+    parser.add_argument('-s', '--host',
+                        required=True,
+                        action='store',
+                        help='vSphere service to connect to')
+
     parser.add_argument('-S', '--disable_ssl_verification',
                         required=False,
                         action='store_true',
                         help='Disable ssl host certificate verification')
 
-    parser.add_argument('-n', '--name',
-                        help='DNS Name of the VirtualMachine to power on')
-
-    parser.add_argument('-D', '--debug',
-                        action='store_true',
-                        help='enable debugging output')
+    parser.add_argument('-u', '--user',
+                        required=True,
+                        action='store',
+                        help='User name to use when connecting to host')
     return parser
 
 
